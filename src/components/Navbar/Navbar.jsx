b0a865252cfc48770edf5e87fa79e05645991387
Amitosh15 <amitosh8090@gmail.com>
2025-12-23 10:29:02 +0530

    Apply style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const menuItem = [
-    { id: "about", label: "About" },
-    { id: "education", label: "Education" },
-    { id: "skills", label: "Skills" },
-    { id: "projects", label: "Projects" },
-  ];
   return (
     <nav className="navbar">
       <div className="navbar-logo">Amitosh</div>
@@ -29,24 +23,6 @@ const Navbar = () => {
         )}
       </button>
 
-      {/* Navbar menu */}
-      {/* <ul className={`navbar-links${isOpen ? " open" : ""}`}>
-        {menuItem.map((item) => (
-          <li
-            key={item.id}
-            className={`nav-list ${
-              activeSection === item.id ? "text-[#38bdf8]" : ""
-            }`}
-          >
-            <button
-              className="nav-btn"
-              onClick={() => handleMenuItemClick(item.id)}
-            >
-              {item.label}
-            </button>
-          </li>
-        ))}
-      </ul> */}
       <ul className={`navbar-links${isOpen ? " open" : ""}`}>
         <Link to={"/about"}>
           <li>About</li>
