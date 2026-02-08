d176e0e51b6b5ebbc4439fb2cacbcd3c26491544
Amitosh15 <amitosh8090@gmail.com>
2026-02-08 11:54:08 +0530

    Removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import About from "./pages/About/About";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Education from "./pages/Education/Education";
-import Skills from "./pages/Skills/Skills";
-import Projects from "./pages/Projects/Projects";
-import Contact from "./pages/Contact/Contact";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
